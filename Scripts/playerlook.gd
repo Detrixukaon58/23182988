db9eb296c8eaf6421d64e855224638d8478e0d08
Hux <droshux@gmail.com>
2023-10-26 15:09:41 +0100

    Added Camera Looking
    However, you can't move and look around at the same time

--- Scripts/playerlook.gd
+++ Scripts/playerlook.gd
@@ -1,12 +1,19 @@
 extends Camera3D
 
 @export var SENSITIVITY: float;
+var player: CharacterBody3D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	player = get_node("..")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_x(deg_to_rad(event.relative.y * -SENSITIVITY))
+		player.rotate_y(deg_to_rad(event.relative.x * -SENSITIVITY))
